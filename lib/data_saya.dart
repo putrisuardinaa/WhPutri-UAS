dc633a7604a4ac72581232a634efcb7f2d840f62
putri <putrisuardina02@gmail.com>
2025-05-31 11:59:15 +0700

    uas selesai

--- lib/data_saya.dart
+++ lib/data_saya.dart
@@ -1,4 +1,5 @@
 class DataSaya {
-   static String nama = "Kudut";
-    static String gambar = "assets/mahasiswa/kudut.png";
+  static String nama = "putri";
+  static String gambar =
+      "assets/putri.jpg";
 }
